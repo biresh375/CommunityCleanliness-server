4978e43032d3b3dc90725f9c917023e66ebb0b8d
Biresh paul <palbiresh375@gmail.com>
2025-11-13 15:05:58 +0600

    after deploy

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("IssueReportingPortal");
     const usersCollection = db.collection("users");
@@ -133,7 +133,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
